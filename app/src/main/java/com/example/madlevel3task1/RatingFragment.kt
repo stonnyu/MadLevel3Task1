9da9e7e98392a0efabfbaa38d907832e8611ee04
Tonny <mrtonnysu@gmail.com>
2020-09-26 23:39:57 +0200

    Finished game rating app

--- app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,15 +14,37 @@ import androidx.navigation.fragment.findNavController
 class RatingFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_rating, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btn_summary.setOnClickListener {
+            navigateToSummary()
+        }
+
+        showRandomAssessableGame()
+    }
+
+    private fun navigateToSummary() {
+        val args = Bundle()
+        args.putFloat(ARG_GAME_RATING, ratingBar.rating)
+        args.putString(ARG_GAME_NAME, lbl_game_title.text.toString())
+
+        findNavController().navigate(R.id.action_ratingFragment_to_summaryFragment, args)
+    }
+
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf(
+            "Red Dead Redemption 2", "Rocket League",
+            "Shadow of the Tombraider"
+        ).random()
+
+        lbl_game_title.text = randomGame
     }
 }
--- app/src/main/java/com/example/madlevel3task1/StartFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/StartFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_start.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -18,11 +18,16 @@ class StartFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_start, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btn_start.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_startFragment_to_ratingFragment
+            )
+        }
     }
 }
